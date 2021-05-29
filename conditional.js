235274d2fa3a062e7be97dd705ca0fa5857d5f42
RAFSANKING <sufianrh12@gmail.com>
2021-05-29 17:28:46 +0600

    first commit

---
+++ conditional.js
@@ -0,0 +1,48 @@
+var shirtPrice = 23;
+if(shirtPrice<20){
+    console.log("I will buy it");
+
+}
+else(
+    console.log("Sorry, it is to much cost")
+)
+var shirtPrice = 20;
+if(shirtPrice==20){
+    console.log("I will buy it");
+
+}
+else(
+    console.log("Sorry, it is to much cost")
+)
+var shirtPrice = 23;
+if(shirtPrice!=20){
+    console.log("I will buy it");
+
+}
+else(
+    console.log("Sorry, it is to much cost")
+)
+var shirtPrice = 20;
+if(shirtPrice!=20){
+    console.log("I will buy it");
+
+}
+else(
+    console.log("Sorry, it is to much cost")
+)
+var job = true;
+var savingAmount = 500000;
+if(job == true && savingAmount > 200000){
+    console.log("You Do Good Job");
+}
+else{
+    console.log("You should improve your skill");
+}
+var job = true;
+var savingAmount = 500000;
+if(job == false || savingAmount > 200000){
+    console.log("You Do Good Job");
+}
+else{
+    console.log("You should improve your skill");
+}
